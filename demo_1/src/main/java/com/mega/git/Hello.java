066f8eb5250cf8744291cf705bb6a03fb69d5347
MegaBytes1024 <3237852627@qq.com>
2022-07-10 15:42:12 +0800

    提交文件

---
+++ demo_1/src/main/java/com/mega/git/Hello.java
@@ -0,0 +1,17 @@
+package com.mega.git;
+
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+/**
+ * @author dwhstudent
+ * @create 2022-07-10-15:31
+ */
+public class Hello {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello");
+
+    }
+
+}
